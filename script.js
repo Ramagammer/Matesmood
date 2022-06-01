3e1f3600c102573111a392373c3d122658ff2ba4
Ramagammer <84947133+Ramagammer@users.noreply.github.com>
2022-05-31 21:06:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload
+
+const atras = document.getElementById('back');
+
+atras.addEventListener('click', e => {
+	history.back();
+});
+
+function iniciarMap() {
+	var coord = {lat: -34.651683, lng: -58.640602};
+	var map = new google.maps.Map(document.getElementById('map'),{
+		zoom: 14,
+		center: coord
+	});
+	var marcador = new google.maps.Marcador({
+		position: coord,
+		map: map
+	});
+}
